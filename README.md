48794dddcd4662f8a4978714dff24e14da840032
William Bryan <wwbryan00@gmail.com>
2020-12-21 23:17:42 -0800

    add question answer array

---
+++ README.md
@@ -0,0 +1,3 @@
+## Acknowledgements
+
+- [Quiz Questions Derived From](https://www.w3schools.com/js/js_quiz.asp)
--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,35 @@
 var highscore = document.querySelector("#highscore");
 var timer = document.querySelector("#timer");
 var question = document.querySelector("#question");
+var container = document.querySelector("#container");
+
+var questionAnswer = {
+    question: [
+        "Inside which HTML element do we put the JavaScript?", 
+        "Where is the correct place to insert a JavaScript?",
+        "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+        "The external JavaScript file must contain the <script> tag.",
+        "How do you write \"Hello World\" in an alert box?",
+        "How do you create a function in JavaScript?"
+    ],
+
+    answer: [
+        "<script>",
+        "Both are correct",
+        "<script src=\"xxx.js\">",
+        "False",
+        "alert(\"Hello World\")",
+        "function myFunction()"
+    ]
+}
+
+
+
+container.addEventListener("click", function(event) {
+    var element = event.target;
+  
+    
+    if (element.matches("button") === true) {
+      
+    }
+  });
